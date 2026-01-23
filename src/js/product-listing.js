917616febf5de944cf838378ed95c5aefd2804bc
vamundse <vamundse@gmail.com>
2026-01-23 15:52:08 +0100

    Fixed double qoutes

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,6 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { getParam } from './utils.mjs';
+import { getParam } from "./utils.mjs";
 
 const category = getParam("category");
 
