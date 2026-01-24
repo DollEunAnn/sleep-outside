3accf60bee78e4fd447c635a8193a41bcdce91ed
DollEunAnn <euniceannhdollete@gmail.com>
2026-01-24 12:08:12 +0800

    partial

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,12 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
-const dataSource = new ProductData("tents");
+// const dataSource = new ProductData("tents");
 
-const element = document.querySelector(".product-list");
+// const element = document.querySelector(".product-list");
 
-const productList = new ProductList("Tents", dataSource, element);
+// const productList = new ProductList("Tents", dataSource, element);
 
-productList.init();
+// productList.init();
 
 loadHeaderFooter();
